15eabc1193bbb5a874db9049a6fa1cf6f2a33cb3
sb878787 <m.sb87.developer@gmail.com>
2025-08-19 19:19:54 +0330

    feat: add variant to button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,27 @@
 import React, { type ComponentProps } from "react";
 
-type TButton = ComponentProps<"button">
+type TVariant = "primary" | "secondary" | "danger" | "warning" | "success";
 
-function Button({ children, ...rest }: TButton) {
-  return <button {...rest} >{children}</button>;
+type TButton = ComponentProps<"button"> & {
+  variant: TVariant;
+};
+
+function Button({ children, variant, style, ...rest }: TButton) {
+  return <button {...rest} style={{ ...style, ...checkVariant(variant)}}>{children}</button>;
 }
 
 export default Button;
+
+function checkVariant(variant: TVariant) {
+  if (variant === "primary") {
+    return { backgroundColor: "blue", color: "white" };
+  } else if (variant === "secondary") {
+    return { backgroundColor: "gray", color: "black" };
+  } else if (variant === "danger") {
+    return { backgroundColor: "red", color: "white" };
+  } else if (variant === "success") {
+    return { backgroundColor: "green", color: "white" };
+  } else if (variant === "warning") {
+    return { backgroundColor: "yellow", color: "white" };
+  }
+}
