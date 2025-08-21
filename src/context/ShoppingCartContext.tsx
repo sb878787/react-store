18a142da474e8fd813524c5698b63ebf59df251b
sb878787 <m.sb87.developer@gmail.com>
2025-08-21 17:51:40 +0330

    feat: create handleIncreaseProductQty function

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -11,6 +11,7 @@ interface ICartItem {
 
 interface IShoppingCartContext {
   cartItems: ICartItem[];
+  handleIncreaseProductQty: (id: number) => void;
 }
 
 export const ShoppingCartContext = createContext({} as IShoppingCartContext);
@@ -18,17 +19,32 @@ export const ShoppingCartContext = createContext({} as IShoppingCartContext);
 export const useShoppingCartContext = () => {
   const context = useContext(ShoppingCartContext);
   if (!context) {
-    throw new Error("useShoppingCartContext must be used within a ShoppingCartProvider");
+    throw new Error(
+      "useShoppingCartContext must be used within a ShoppingCartProvider"
+    );
   }
 
   return context;
-}
+};
 
 export function ShoppingCartProvider({ children }: IShoppingCartContextType) {
   const [cartItems, setCartItems] = useState<ICartItem[]>([]);
 
+  const handleIncreaseProductQty = (id: number) => {
+    setCartItems((prevProducts) => {
+      const existingProduct = prevProducts.find((product) => product.id === id);
+      if (existingProduct) {
+        return prevProducts.map((product) =>
+          product.id === id ? { ...product, qty: product.qty + 1 } : product
+        );
+      } else {
+        return [...prevProducts, { id, qty: 1 }];
+      }
+    });
+  };
+
   return (
-    <ShoppingCartContext.Provider value={{ cartItems }}>
+    <ShoppingCartContext.Provider value={{ cartItems, handleIncreaseProductQty }}>
       {children}
     </ShoppingCartContext.Provider>
   );
